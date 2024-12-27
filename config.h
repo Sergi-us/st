537235df7bf338f2200246dc021b45e0231f2fe8
Sergius M. <sergius@posteo.de>
2024-12-27 19:01:31 -0300

    schriftart

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NF ExtraLight:style=ExtraLight:pixelsize=26:antialias=true:autohint=true";
+static char *font = { "JetBrainsMono NF ExtraLight:style=ExtraLight:pixelsize=28:antialias=true:autohint=true" };
 static char *font2[] = { "OpenMoji:pixelsize=26:antialias=true:autohint=true" };
 static int borderpx = 2;
 
