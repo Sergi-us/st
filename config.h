23dbe0c40b09a0e37ba053084bfa7426d8c0e3c9
Sergius M. <sergius@posteo.de>
2025-08-08 22:45:05 +0200

    Schriftgrößen angepasst

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = { "JetBrainsMono NF:style=Light:size=16:antialias=true:autohint=true:hintstyle=hintslight" };
-static char *font2[] = { "JetBrainsMono NF:style=Light:size=16:antialias=true:autohint=true:hintstyle=hintslight" };
-static int borderpx = 8;
+static char *font = { "JetBrainsMono NF:style=Light:size=11:antialias=true:autohint=true:hintstyle=hintslight" };
+static char *font2[] = { "JetBrainsMono NF:style=Light:size=11:antialias=true:autohint=true:hintstyle=hintslight" };
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -229,13 +229,13 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 1} },
-	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 1} },
-	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
+	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 1}           },
+	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 1}           },
+	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},          1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
+	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"}      },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"}      },
 };
 
 /* Internal keyboard shortcuts. */
@@ -272,8 +272,8 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
-	{ MODKEY,		XK_s,		changealpha,	{.f = -0.05} },
-	{ MODKEY,		XK_a,		changealpha,	{.f = +0.05} },
+	{ MODKEY,               XK_s,           changealpha,    {.f = -0.05} },
+	{ MODKEY,               XK_a,           changealpha,    {.f = +0.05} },
 	{ TERMMOD,              XK_Up,          zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
